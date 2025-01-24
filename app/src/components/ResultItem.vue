41309cdf57527cff3b7d7665a2461f612bd5a4e6
Nikita Albert <nikitaalbert@gmail.com>
2025-01-24 12:09:47 -0600

    Various UI updates

--- app/src/components/ResultItem.vue
+++ app/src/components/ResultItem.vue
@@ -13,13 +13,14 @@
         <div class="result-banner" @click="showContent = !showContent">
 
         </div>
+            <div class="result-content-default">
 
-        <div class="result-content-default">
+            </div>
+            <div v-if="showContent" class="result-content-expanded" >
+                <div class="result-secondary-content-container">
 
-        </div>
-        <div class="result-content-expanded" v-if="showContent">
-
-        </div>
+                </div>
+            </div>
     </div>
 </template>
 
@@ -27,25 +28,37 @@
 
 .result-card {
     flex: 1;
-    flex-flow: column;
+    flex-direction: column;
     margin: 25px 25px 0px 25px;
     border: rgb(181, 247, 207);
 }
 
 .result-banner {
-    background-color: darkolivegreen;
+    background-color: rgb(26, 117, 84);
     flex: 1;
     height: 30px;
 }
 
 .result-content-default {
-    background-color: rgb(181, 247, 207);
+    background-color: rgb(34, 185, 130);
     height: 110px
 }
 
 .result-content-expanded {
-    background-color: rgb(181, 247, 207);
-    height: 110px
+    background-color: rgb(34, 185, 130);
+    height: 110px;
+    flex-direction: column;
+    align-items: center;
+
+}
+
+.result-secondary-content-container {
+    background-color: rgb(206, 212, 210);
+    margin-left: auto;
+    margin-right: auto;
+    min-width: 90%;
+    max-width: fit-content;
+    height: 85px;
 }
 
 </style>
