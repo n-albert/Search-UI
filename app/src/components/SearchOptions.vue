398bbf6b33ea6d8bd9cef912329e6fba7c3672e7
Nikita Albert <nikitaalbert@gmail.com>
2025-02-06 13:43:41 -0600

    Made some updates to change how radio button selections look like.

--- app/src/components/SearchOptions.vue
+++ app/src/components/SearchOptions.vue
@@ -1,11 +1,16 @@
 <template>
-    <div class="search-options">
+    <div class="search-options-container">
         <div>Selected search type: {{ searchSelection }}</div>
-        <input type="radio" v-model="searchSelection" id="standards" value="Standards" />
-        <label for="standards">Standards</label><br />
-
-        <input type="radio" v-model="searchSelection" id="prodcedures" value="Procedures" />
-        <label for="procedures">Procedures</label><br />
+        <div class="search-options">    
+            <div class="search-option" :class="{ 'selected': searchSelection === 'Standards' }">
+                <input type="radio" v-model="searchSelection" id="standards" value="Standards" />
+                <label for="standards">Standards</label><br />
+            </div>
+            <div class="search-option" :class="{ 'selected': searchSelection === 'Procedures' }">
+                <input type="radio" v-model="searchSelection" id="procedures" value="Procedures" />
+                <label for="procedures">Procedures</label><br />
+            </div>      
+        </div>
     </div>
 </template>
 
@@ -19,3 +24,32 @@
         },
     };
 </script>
+
+<style>
+    .search-options-container {
+        min-height: 50px;
+    }
+    .search-options {
+        display: flex;
+    }
+    .search-option {
+        width: 150px;
+        height: 25px;
+        border: 2px solid gray;
+        text-align: center;
+    }
+
+    .search-option input {
+        opacity: 0;
+    }
+
+    .search-option label {
+        position: relative;
+        left: -6.5px;
+    }
+
+    .selected {
+        background-color: rgb(0, 189, 126);
+        color: black;
+    }
+</style>
